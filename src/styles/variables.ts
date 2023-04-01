cbacc7ecb487e135ab06a1a80e4ebb61d9c08bc0
eduardoqsilva <eduadsilva@outlook.com>
2023-04-01 01:14:14 -0300

    create a varibles for styled-components"

---
+++ src/styles/variables.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  orange:'#EB5E28',
+  orange_light: '#FF7541',
+  orange_dark: '#783116',
+
+  red: '#F03847',
+  red_dark: '#7A1921',
+  
+  green_light: '#8FF7A7',
+
+  yellow: '#F2C14E',
+
+  gray1: '#121214',
+  gray2: '#202024',
+  gray3: '#323238',
+  gray4: '#7C7C8A',
+  gray5: '#8D8D99',
+  gray6: '#C4C4CC',
+  gray7: '#E1E1E6',
+
+  white: '#FFFF'
+}
+
+export const font = {
+  default: 'Roboto, sans-serif'
+}
