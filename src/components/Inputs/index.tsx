b05efcc58b64f91ee02c2f1441b73b2c54150389
eduardoqsilva <eduadsilva@outlook.com>
2023-04-02 23:53:10 -0300

    add validation on inputs

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -1,30 +1,67 @@
 import { InputWrapperStyled } from "./inputs.styled";
 import { IconContext, IconProps } from "@phosphor-icons/react";
-import React, { useState, FocusEvent } from "react";
+import React, { useState, FocusEvent, useEffect } from "react";
 import * as regex from "../../constants/regex"
+import * as msg from "../../constants/inputErrorMessages";
 
+type SetState = React.Dispatch<React.SetStateAction<any>>;
 interface TextInputType {
   icon: React.ReactElement<IconProps>
-  typeOf: 'email' | 'text' | 'number' | 'date' | 'password'
+  type: 'email' | 'text' | 'number' | 'date' | 'password'
   placeholder: string
   label: string
+  characters?: {min: number, max: number}
+  getValue: [SetState, any, string] | SetState
+  requered?: boolean
 }
-export function Input({ icon, typeOf, placeholder, label }: TextInputType) {
+
+export function Input({ icon, type, placeholder, label, characters, getValue, requered}: TextInputType) {
 
   const [warning, setWarning] = useState(false)
   const [value, setValue] = useState('')
+  
+  const typeOfRegex = regex[type as keyof typeof regex]
+
+
+  useEffect(() => {
+    if(Array.isArray(getValue)) {
+      const [setValueOut, valueOut, key] = getValue
+      setValueOut({
+        ...valueOut,
+        [key]: value 
+      })
+    }else {
+      getValue(value)
+    }
+  },[value])
 
   function handleSetValue(e:FocusEvent<HTMLInputElement>) {
-    const value = e.currentTarget.value.trim()
+    const value = (
+        type === 'email' 
+        ?e.currentTarget.value.trim().toLowerCase()
+        : e.currentTarget.value.trim()
+    )
 
-    if(regex.mail.test(value) || value === '') {
-      console.log(value)
-      setWarning(false)
+    if(typeOfRegex.test(value)) {
+      if(characters !== undefined) {
+        if(value.length >= characters.min) {
+          setValue(value)
+          setWarning(false)
+        } else {
+          setWarning(true)
+        }
+      } else {
+        setWarning(false)
+        setValue(value)
+      }
     } else {
       setWarning(true)
+      console.log('não passou na regex')
     }
   }
-
+  function setErrorMessage(e: React.FormEvent<HTMLInputElement>) {
+    e.currentTarget.setCustomValidity(msg[type as keyof typeof msg])
+  }
   return (
     <InputWrapperStyled warning={warning}>
       <IconContext.Provider value={{
@@ -38,9 +75,12 @@ export function Input({ icon, typeOf, placeholder, label }: TextInputType) {
 
       <label>{label}</label>
       <input 
-        placeholder={placeholder}
-        type={typeOf}
         onBlur={handleSetValue}
+        onInvalid={setErrorMessage}
+        placeholder={placeholder}
+        type={type}
+        maxLength={characters?.max}
+        required={requered}
       />
     </InputWrapperStyled>
   )
