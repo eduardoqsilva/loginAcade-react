544f91a7b9b1d60d652e21e4e8500e5d175efc4d
eduardoqsilva <eduadsilva@outlook.com>
2023-04-03 22:31:45 -0300

    create a inputErrorMessages.ts

---
+++ src/constants/inputErrorMessages.ts
@@ -0,0 +1,9 @@
+export const email = 'O campo e-mail precisa ser válido'
+
+export const password = 'O campo senha deve conter ao menos 8 e no máximo 16 caracteres'
+
+export const date = 'O campo data precisa de uma data válida'
+
+export const text = 'O campo deve conter apenas letras e não pode ser vazio'
+
+export const number = 'O campo deve conter apenas números'
