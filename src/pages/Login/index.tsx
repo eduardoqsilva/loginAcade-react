67c95b4317333c31fdae716cdf0b2da847aa9f70
eduardoqsilva <eduadsilva@outlook.com>
2023-04-05 17:37:51 -0300

    update login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,9 @@
-import { At, Barbell, Calendar, Lock, Ruler, User } from "@phosphor-icons/react"
+import { At, Barbell, Cake, Calendar, GenderIntersex, Lock, MaskHappy, Ruler, User } from "@phosphor-icons/react"
 import { useEffect, useState } from "react"
 import { Button } from "../../components/Buttons"
-import { Input } from "../../components/Inputs"
+import { Dropdown, Input } from "../../components/Inputs"
+import { Logo } from "../../components/Logo"
+import { ContainerLoginStyled, FormStyled } from "./login.styled"
 
 export function Login() {
   const [values, setValues] = useState({
@@ -11,64 +13,80 @@ export function Login() {
     date: '',
     height: '',
     weight: '',
+    sex: ''
   })
   useEffect(() => {
     console.log(values)
   },[values])
 
   return (
-    <form action="">
-      <Input
-        icon={<User/>}
-        type={'text'}
-        label={'Nome'}
-        placeholder={'José Arrantes'}
-        getValue={[setValues, values, 'name']}
-        requered={true}
-      />
-      <Input 
-        icon={<At/>}
-        type={'email'}
-        label={'E-mail'}
-        placeholder={'seumelhoremail@email.com'}
-        getValue={[setValues, values, 'email']}
-        requered={true}
-      />
-      <Input 
-        icon={<Lock/>}
-        type={'password'}
-        label={'Senha'}
-        placeholder={'••••••••••••'}
-        getValue={[setValues, values, 'password']}
-        characters={{min: 8, max: 16}}
-        requered={true}
-      />
-      <Input 
-        icon={<Calendar/>}
-        type={'date'}
-        label={'Data de nascimento'} 
-        getValue={[setValues, values, 'date']}
-        requered={true}
-      />
-      <Input 
-        icon={<Ruler/>}
-        type={'number'}
-        label={'altura'} 
-        getValue={[setValues, values, 'height']}
-        characters={{min: 4, max: 4}}
-        placeholder={'1,78'}
-        requered={true}
-      />
-      <Input 
-        icon={<Barbell/>}
-        type={'number'}
-        label={'Peso'} 
-        getValue={[setValues, values, 'weight']}
-        characters={{min: 2, max: 5}}
-        placeholder={'70,5kg'}
-        requered={true}
-      />
-      <Button text="Finalizar"/>
-     </form>
+    <ContainerLoginStyled>
+      <Logo width={100}/>
+      <FormStyled action="">
+        <h1>Cadastro</h1>
+        <div className="form">
+          <Input
+            icon={<User/>}
+            type={'text'}
+            label={'Nome'}
+            placeholder={'José Arrantes'}
+            getValue={[setValues, values, 'name']}
+            requered={true}
+          />
+          <Input
+            icon={<At/>}
+            type={'email'}
+            label={'E-mail'}
+            placeholder={'seumelhoremail@email.com'}
+            getValue={[setValues, values, 'email']}
+            requered={true}
+          />
+          <Input
+            icon={<Lock/>}
+            type={'password'}
+            label={'Senha'}
+            placeholder={'••••••••••••'}
+            getValue={[setValues, values, 'password']}
+            characters={{min: 8, max: 16}}
+            requered={true}
+          />
+          <div className="grid">
+            <Input
+              icon={<Cake/>}
+              type={'date'}
+              label={'Data de nascimento'}
+              getValue={[setValues, values, 'date']}
+              requered={true}
+            />
+            <Input
+              icon={<Ruler/>}
+              type={'number'}
+              label={'altura'}
+              getValue={[setValues, values, 'height']}
+              characters={{min: 4, max: 4}}
+              placeholder={'1,78'}
+              requered={true}
+            />
+            <Input
+              icon={<Barbell/>}
+              type={'number'}
+              label={'Peso'}
+              getValue={[setValues, values, 'weight']}
+              characters={{min: 2, max: 5}}
+              placeholder={'70,5kg'}
+              requered={true}
+            />
+            <Dropdown 
+              icon={<GenderIntersex />}
+              label={"Sexo"}
+              requered={true}
+              getValue={[setValues, values, 'sex']}
+              itens={['M', 'F']}
+            />
+          </div>
+        </div>
+        <Button text="Finalizar"/>
+      </FormStyled>
+    </ContainerLoginStyled>
   )
 }
