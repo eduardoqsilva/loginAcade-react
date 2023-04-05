fa3e16f228a4ec6dee6a8efe302f98f9b51d0e29
eduardoqsilva <eduadsilva@outlook.com>
2023-04-05 17:39:43 -0300

    create a component logo

---
+++ src/components/Logo/logo.styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { colors } from "../../styles/variables";
+
+interface LogoTypes {
+  color: '1' | '2';
+  width: number;
+}
+
+export const LogoStyled = styled.div<LogoTypes>`
+  width: ${props => props.width}px;
+  height: ${props => props.width}px;
+
+  & .icon {
+    height: 100%;
+    width: 100%;
+    color: ${props => props.color === '1' 
+      ? `${colors.orange}`
+      : `${colors.white}`
+    };
+  }
+`
