35a1a3a3d80fdd1a8c19092c8065df76111b986f
eduardoqsilva <eduadsilva@outlook.com>
2023-04-08 17:53:05 -0300

    Change routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,8 +5,8 @@ import { Login } from "../pages/Login"
 export function Router() {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Register />} />
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
