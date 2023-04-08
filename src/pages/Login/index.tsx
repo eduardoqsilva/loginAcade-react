fcdf7c1748105dd987d0b9caf9b19a32fe7b43f2
eduardoqsilva <eduadsilva@outlook.com>
2023-04-08 20:00:53 -0300

    add loading and update styles and create functions

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,70 @@
+import { At, LockKey } from "@phosphor-icons/react";
+import { FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../components/Buttons";
+import { Input } from "../../components/Inputs";
+import { Loading } from "../../components/Loading";
+import { Logo } from "../../components/Logo";
+import { BackgroundStyled, ContainerStyled, FormStyled, WrapperLoginStyled } from "./login.styled";
+
 export function Login() {
+
+  const navigate = useNavigate()
+  const [load, setLoad] = useState(false)
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  function handleOnSubmit(e:FormEvent) {
+    e.preventDefault()
+  }
+  function handleRegister() {
+    setLoad(true)
+    setTimeout(() =>{
+      navigate('/register')
+    }, 2000)
+  }
+
   return (
-    <h1>Login</h1>
+    <>
+      <Loading loading={load}/>
+      <BackgroundStyled />
+      <ContainerStyled>
+        <WrapperLoginStyled>
+          <Logo width={130}/>
+          <FormStyled onSubmit={handleOnSubmit}>
+            <h2>Login</h2>
+            <Input 
+              icon={<At/>}
+              label={'EMAIL'}
+              getValue={[setValues, values, 'email']}
+              type={'email'}
+              placeholder={'seuemailaqui@mail.com'}
+              variation={'02'}
+            />
+            <Input 
+              icon={<LockKey/>}
+              label={'SENHA'}
+              getValue={[setValues, values, 'email']}
+              type={'password'}
+              placeholder={'••••••••••••'}
+              variation={'02'}
+            />
+            <div className="buttons">
+              <Button 
+                text="Login"
+                type="submit"
+              />
+              <Button 
+                click={handleRegister}
+                text="Cadastro"
+                variation="secundary"
+              />
+            </div>
+          </FormStyled>
+        </WrapperLoginStyled>
+      </ContainerStyled>
+    </>
   )
 }
--- src/pages/Login/login.styled.ts
+++ src/pages/Login/login.styled.ts
@@ -1,1 +1,67 @@
 import styled from "styled-components";
+
+import background from "../../assets/imgs/login/background.jpg"
+import { colors, font } from "../../styles/variables";
+
+export const BackgroundStyled = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  background: linear-gradient(rgba(0, 0, 0, 0.82), rgba(0, 0, 0, 0.82)), url(${background});
+  background-size: cover;
+  background-position: 70% top;
+  background-repeat: no-repeat;
+  filter: grayscale(100%) brightness(1.1);
+`
+
+export const ContainerStyled = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  
+  padding: 1rem;
+`
+export const WrapperLoginStyled = styled.header`
+  width: 100%;
+  height: 100%;
+  max-width: 400px;
+  margin: auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 6rem;
+`
+
+export const FormStyled = styled.form`
+  width: 100%;
+
+  & h2 {
+    color: ${colors.gray7};
+    font-family: ${font.default};
+    font-size: 1.5rem;
+    font-weight: 600;
+    line-height: 28px;
+    letter-spacing: 0.18em;
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+
+  & .buttons {
+    width: 300px;
+    margin: auto;
+    margin-top: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1.56rem;
+  }
+`
