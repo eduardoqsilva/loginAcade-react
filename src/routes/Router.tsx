7d4461aedbd465382b448adfb78555709dfc0030
eduardoqsilva <eduadsilva@outlook.com>
2023-04-10 23:23:30 -0300

    add route terms

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import { Register } from "../pages/Register"
 import { Login } from "../pages/Login"
+import { Terms } from "../pages/Terms"
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/terms" element={<Terms />} />
     </Routes>
   )
 }
