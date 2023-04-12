48f11de7749f795430f2e8960bb99a34f9f0b339
eduardoqsilva <eduadsilva@outlook.com>
2023-04-11 21:50:15 -0300

    create a new route home

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import { Register } from "../pages/Register"
 import { Login } from "../pages/Login"
 import { Terms } from "../pages/Terms"
+import { Home } from "../pages/Home"
 
 export function Router() {
   return (
@@ -9,6 +10,7 @@ export function Router() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/terms" element={<Terms />} />
+      <Route path="/Home" element={<Home />} />
     </Routes>
   )
 }
